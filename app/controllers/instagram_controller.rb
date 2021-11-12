d56f28e9f588f1c47ae0ce9b459a7fa04231fd1d
ta-mu-aa <76101803+ta-mu-aa@users.noreply.github.com>
2021-11-12 23:34:18 +0900

    instagram graph api の設定完了

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,5 +1,22 @@
 class InstagramController < ApplicationController
     def home
-        
+        instagram_business_id = '17841448718565047'
+        access_token = 'EAADVEC1yFhgBAGZC7HoW7v5fKTZAynqHPii0vZAMCCZA0z2ZBp7HFMziOqQelxgnh7QnAO3L5uwRCU3jSs2OHx1mZB9z8sU8ipYVNqflYwxAVJJOSOgciJ96x7VPCQfktMQTEeaGHO0ze6op3aP67PvqHHSF25GdcpQRgzZAzgvZCVTAUuSpivwp'
+        target_user = 'ehime_log'
+
+# 自分のアカウントのデータを取得
+        query = 'name,media{caption,like_count,media_url,permalink,timestamp,username}&access_token='+access_token
+
+        instagram_api_url = 'https://graph.facebook.com/v12.0/'
+        target_url = instagram_api_url+instagram_business_id+"?fields="+query+"&access_token="+access_token
+
+        # ch = curl_init()
+        # curl_setopt(ch, CURLOPT_URL, target_url)
+        # curl_setopt(ch, CURLOPT_RETURNTRANSFER, true)
+
+        # instagram_data = curl_exec(ch)
+        # curl_close(ch)
+
+        # @instagram_data;
     end
 end
