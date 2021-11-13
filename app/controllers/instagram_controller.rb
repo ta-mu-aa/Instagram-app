1679febe6a007c1346bbd180acccfad7f2a272b0
ta-mu-aa <76101803+ta-mu-aa@users.noreply.github.com>
2021-11-13 21:25:49 +0900

    APIから取得したデータを表示まで

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,23 +1,21 @@
 class InstagramController < ApplicationController
+    require 'httpclient'
+    require 'json'
+
     def home
         instagram_business_id = ENV["INSTAGRAM_BUSINESS_ID"]
         access_token = ENV["ACCESS_TOKEN"]
         target_user = ENV["TARGET_USER"]
-
-# 自分のアカウントのデータを取得
+        # 自分のアカウントのデータを取得
         query = "name,media{caption,like_count,media_url,permalink,timestamp,username}&access_token=#{access_token}"
-
         instagram_api_url = 'https://graph.facebook.com/v12.0/'
-        # @target_url = instagram_api_url+instagram_business_id+"?fields="+query+"&access_token="+access_token
-        @target_url = "#{instagram_api_url}#{instagram_business_id}+?fields=+#{query}+&access_token=#{access_token}"
+        target_url = "#{instagram_api_url}#{instagram_business_id}?fields=#{query}&access_token=#{access_token}"
 
-        # ch = curl_init()
-        # curl_setopt(ch, CURLOPT_URL, target_url)
-        # curl_setopt(ch, CURLOPT_RETURNTRANSFER, true)
-
-        # instagram_data = curl_exec(ch)
-        # curl_close(ch)
-
-        # @instagram_data;
+        # 新しいインスタンスを作成
+        client = HTTPClient.new
+        #APIリクエスト
+        res = client.get(target_url)
+        #ハッシュ化して返す
+        @result_json =  JSON.parse(res.body)
     end
 end
