f0c3f0df16c4fd63620b4d899a5fb0059f62aa3a
ta-mu-aa <76101803+ta-mu-aa@users.noreply.github.com>
2021-12-19 22:44:12 +0900

    全体調整

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -3,7 +3,7 @@ class InstagramController < ApplicationController
     require 'json'
 
     def home
-
+        
     end
 
     def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'instagram#home'
-  get 'instagram/post_list'
+  get 'instagram/post_list',to: 'instagram#post_list'
   get 'instagram/about'
 end
